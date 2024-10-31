6b923324241fb991c6f74179ae8d780615832288
ronnyfph <ronnyfph@gmail.com>
2024-10-31 13:16:09 +0100

    Add bringaud_siders.csv to data folder
    This commit introduces bringaud_siders.csv with transposable element data for various chromosomes. The file contains 1853 entries with fields for ID, chromosome, name, family, strand, start, end, and chromosome size.

---
+++ 1.Modifying_data/1.Extract_PDF_data.py
@@ -0,0 +1,38 @@
+import pdfplumber
+import pandas as pd
+from seaborn.external.docscrape import header
+
+
+def extract_text_from_all_pages_with_layout(pdf_path):
+    all_text = []
+    with pdfplumber.open(pdf_path) as pdf:
+        for page in pdf.pages:
+            all_text.append(page.extract_text())
+    return "\n\n".join(all_text)  # Use double newlines to indicate a new page
+
+def text_to_dataframe(text):
+    lines = text.split('\n')
+    # Customize this part based on the specific format of your table
+    data = [line.split() for line in lines]
+    columns = data[0]
+    rows = data[1:]
+    df = pd.DataFrame(data=rows, columns=columns)
+    df.dropna(inplace=True)  # Remove the 'new page' marker, that is a residue of '\n\n'
+    return df
+
+
+def save_dataframe_to_csv(df, csv_path):
+    df.to_csv(csv_path, index=False, header=True, sep=',')
+
+
+def main():
+    pdf_path = '../data/Bringaud_SIDERs.pdf'
+    csv_path = '../data/bringaud_siders.csv'
+
+    text = extract_text_from_all_pages_with_layout(pdf_path)
+    df = text_to_dataframe(text)
+    save_dataframe_to_csv(df, csv_path)
+
+
+if __name__ == '__main__':
+    main()
